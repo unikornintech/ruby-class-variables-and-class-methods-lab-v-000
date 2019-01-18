ba366a9bf2c5bf7fc9238c0f9144ed68252a1711
Lauren Days <lmchambers0420@gmail.com>
2019-01-18 21:51:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,33 +1,37 @@
-class Song 
-  attr_accessor :name, :artist, :genre 
-  
-  @@count = 0 
-  @@artists = []
-  @@genre = []
-  
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    
-    @@count += 1 
-    @@artists << artist
-    @@genres << genre
-  end 
-  
-  def self.count
-    @@count
-  end 
-  
-  def self.artists
-    @@artists.uniq 
-  end 
-  
-  def self.genres
-    @@genres.uniq 
-  end
-  
-  def self.genre_count
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+      @name = name
+      @artist = artist
+      @genre = genre
+
+      @@count += 1
+      @@artists << artist
+      @@genres << genre
+
+    end
+
+    def self.count
+      @@count
+
+    end
+
+    def self.artists
+      @@artists.uniq
+
+    end
+
+    def self.genres
+      @@genres.uniq
+
+    end
+
+    def self.genre_count
       genre_count ={}
       @@genres.each do |genre|
 
@@ -61,5 +65,4 @@ class Song
       
       artist_count
     end
-  
-end 
+  end 
